550f9d71143bbee08b662fd9112c43b31cd1baf5
Vankka <vankka.main@gmail.com>
2022-03-23 19:51:07 +0200

    Add modal component

---
+++ components/modal.jsx
@@ -0,0 +1,19 @@
+import styles from '../styles/modal.module.css'
+
+export default function Modal({ title, open, close, children }) {
+    return (
+        <div style={{display: open ? "flex" : "none"}}>
+            <div className={styles.modal}>
+                <div className={styles.modalWrapper}>
+                    <div className={styles.modalHeader}>
+                        <h2>{title}</h2>
+                        <button onClick={() => close()}>&times;</button>
+                    </div>
+                    <div className={styles.modalContent}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
