db111c10f007800e7842157ae4b61c4d703263d3
Vankka <vankka.main@gmail.com>
2023-12-07 22:14:06 +0200

    New MinecraftAuth application id

--- app/link/page.jsx
+++ app/link/page.jsx
@@ -4,7 +4,7 @@ export default function LinkPage({ searchParams }) {
     let { method } = searchParams ?? {};
 
     let host = "https://minecraftauth.me";
-    let application = encodeURIComponent("HIKzPIRfgil7qAnA");
+    let application = encodeURIComponent("oed3dcGDezjxUqAG");
     let redirectUrl = encodeURIComponent("https://discordsrv.vankka.dev/linking-success");
     let scopes = encodeURIComponent("minecraft discord");
 
