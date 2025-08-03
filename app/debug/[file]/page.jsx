c277e1c7c4bfa2efb083fad1fcc45bfbcc40c888
Vankka <vankka.main@gmail.com>
2025-08-03 19:54:26 +0300

    Fix unnecessary useParams in server side component

--- app/debug/[file]/page.jsx
+++ app/debug/[file]/page.jsx
@@ -1,11 +1,9 @@
 import DebugClient from "./client";
-import {useParams} from "next/navigation";
 
 export const metadata = {
     title: "DiscordSRV | Debug report"
 }
 
 export default function Page() {
-    const params = useParams();
-    return <DebugClient params={params}/>
+    return <DebugClient />
 }
